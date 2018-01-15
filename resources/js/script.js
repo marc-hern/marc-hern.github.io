846687ada3551db4e2603b635ff20b8a441a30d4
marc-hern <csplayer13>
2018-01-14 17:50:57 -0800

    Splash, aboutme, contact w/ css w/ scripts

---
+++ resources/js/script.js
@@ -0,0 +1,95 @@
+(function($) {
+    'use strict';
+    
+    /* ----------------------------------------------- */
+    /* PRELOADER                                       */
+    /* ----------------------------------------------- */
+    $(window).on('load', function() {
+        $('#preloader').delay(300).fadeOut('slow', function() {
+            $(this).remove();
+        });
+    });
+    
+    
+    
+    $(document).ready(function() {
+        
+        /* ----------------------------------------------- */
+        /* BACKGROUND MENU ON SCROLL                       */
+        /* ----------------------------------------------- */
+        /* Adds and removes sticky-menu class when scrolling */
+        $(window).on('scroll', function() {
+            var menu_area = $('.menu-area');
+            if ($(window).scrollTop() > 200){
+                menu_area.addClass('sticky-menu');
+            } else {
+                menu_area.removeClass('sticky-menu');
+            }
+        });
+        /* Background scroll end -->
+        
+        
+        
+        /* ----------------------------------------------- */
+        /* NAVIGATION COLLAPSE                             */
+        /* ----------------------------------------------- */
+        /* Detects the enter/exit of elements in the viewport when the user scrolls.
+           Target is navbar-collapse */
+        $(document).on('click', '.navbar-collapse.in', function(e){
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+        /* Navigation collapse end -->
+        
+        
+        
+        /* ----------------------------------------------- */
+        /* SMOOTH SCROLL                                   */
+        /* ----------------------------------------------- */
+        $('a.smooth_scroll').on("click", function(e){
+            e.preventDefault();
+            
+            var anchor = $(this);
+            
+            $('html, body').stop().animate( {
+                scrollTop: $(anchor.attr('href')).offset().top - 50}, 1000);
+        });
+        /* Smooth scroll end */
+        
+        
+        /* ----------------------------------------------- */
+        /* TYPED.JS                                        */
+        /* ----------------------------------------------- */
+        /* Typed.js Library for animated typing of pre-defined strings
+           strings: words to be typed
+           typeSpeed: speed of animation
+           loop: continuous playback */
+        var element = $(".typejs");
+        $(function() {
+            element.typed({
+                strings: ["web applications.", "android applications.", "anything :)."],
+                typeSpeed: 80,
+                loop: true,
+            });
+        });
+        /* Typed.js end */
+        
+        
+        /* ----------------------------------------------- */
+        /* MIXITUP.JS                                      */
+        /* ----------------------------------------------- */
+        /* Mixitup.js Dependency-free library for animated organization */
+        $('.portfolio').mixItUp();
+        /* Mixitup end */
+        
+        
+        
+        
+    
+    });  // End of $(document).ready
+})(jQuery);
