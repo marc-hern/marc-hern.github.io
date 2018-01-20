0132ac82a426b2862ac56d8b84695fe5fbd3b45b
marc-hern <csplayer13>
2018-01-20 10:56:06 -0800

    Stylized error and success of form

--- resources/js/script.js
+++ resources/js/script.js
@@ -107,12 +107,12 @@
                 
                 success: function(data) {
                     $contactForm.find('.alert--loading').hide();
-                    $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+                    $contactForm.append('<div class="alert alert--success">Message sent.  Thank you!</div>');
                 },
                 
                 error: function(err) {
                     $contactForm.find('.alert--loading').hide();
-                    $contactForm.append('<div class="alert alert--error">Oops, there was an error!</div>');
+                    $contactForm.append('<div class="alert alert--error">Oops, there was an error!  Please recheck fields.</div>');
                 }
             });
         });
