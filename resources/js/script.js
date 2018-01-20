289e3ea265506590352f9f6d9c74778d1fe286a3
marc-hern <csplayer13>
2018-01-20 10:36:05 -0800

    Testing ajax with form

--- resources/js/script.js
+++ resources/js/script.js
@@ -88,6 +88,35 @@
         /* Mixitup end */
         
         
+        /* ----------------------------------------------- */
+        /* AJAX                                      */
+        /* ----------------------------------------------- */
+        var $contactForm = $('#ajax-contact');
+        $contactForm.submit(function(e) {
+            e.preventDefault();
+            
+            $.ajax({
+                url: '//formspree.io/mar.hernandez010@gmail.com',
+                dataType: 'json',
+                method: 'POST',
+                data: $(this).serialize(),
+                
+                beforeSend: function() {
+                    $contactForm.append('<div class="alert alert--loading">Sending message...</div>');
+                },
+                
+                success: function(data) {
+                    $contactForm.find('.alert--loading').hide();
+                    $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+                },
+                
+                error: function(err) {
+                    $contactForm.find('.alert--loading').hide();
+                    $contactForm.append('<div class="alert alert--error">Oops, there was an error!</div>');
+                }
+            });
+        });
+        
         
         
     
